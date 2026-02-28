9f9a481d4a5623b8332d4e3dc83ebfe720772c09
Mateo521 <mateovillalva42@gmail.com>
2026-02-28 19:18:09 -0300

    v0

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,6 +3,7 @@
 namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
+use Illuminate\Support\Facades\URL;  
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -19,6 +20,10 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        //
+        // 2. Agregar esta validación
+     
+        if (request()->header('x-forwarded-proto') === 'https' || app()->environment('production')) {
+            URL::forceScheme('https');
+        }
     }
 }
